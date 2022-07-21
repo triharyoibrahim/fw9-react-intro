276c0cb5619d1a491d5e84f5ab744d39d786005a
triharyoibrahim <rizkitriharyoibrahim@gmail.com>
2022-07-21 08:20:11 +0700

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://rickandmortyapi.com/api/character",
+});
+
 class App extends React.Component {
+  state = {
+    count: 0,
+    biodata: [],
+  };
+
   constructor() {
     super();
-    this.state = {
-      count: 0,
-    };
+    this.getBiodata();
   }
 
+  getBiodata = async () => {
+    let data = await api.get("/").then(({ data }) => data);
+    this.setState({ biodata: data });
+  };
   increaseCounter = () => {
     this.setState({
       count: this.state.count + 1,
@@ -28,6 +41,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        {this.biodata.map((biodata) => (
+          <div>{biodata.name}</div>
+        ))}
         <h1>{this.state.count}</h1>
         <button onClick={this.decreaseCounter}>-</button>
         <button onClick={this.resetCounter}>reset</button>
